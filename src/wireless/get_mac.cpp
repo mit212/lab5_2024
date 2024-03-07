61a0a636698e7f30a87ff62420cab60f06cab9df
Joseph Ntaimo <jntaimo@gmail.com>
2024-03-07 09:05:00 -0500

    Made it print repeatedly

--- src/wireless/get_mac.cpp
+++ src/wireless/get_mac.cpp
@@ -5,8 +5,11 @@
 void setup(){
   Serial.begin(115200);
   WiFi.mode(WIFI_MODE_STA);
-  Serial.println(WiFi.macAddress());
+  
 }
  
 void loop(){
+  
+  Serial.println(WiFi.macAddress());
+  delay(250);
 }
